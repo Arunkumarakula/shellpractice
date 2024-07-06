b6f1977e12b10d7d3907eef8ecad23c0c97361a9
Akulaarunkumar <arunkumarakula58@gmail.com>
2024-07-06 15:47:51 +0530

    variable2

--- variable2.sh
+++ variable2.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 
-person1 = $1
-person2 = $2
+person1=$1
+person2=$2
 
-echo "$person1:Hi $person2"
-echo "$person2:yeah..!"
-echo "$person1:I'm gd"
-echo "$person2:crazy"
+echo "$person1: Hi $person2"
+echo "$person2: yeah..!"
+echo "$person1: I'm gd"
+echo "$person2: crazy"
